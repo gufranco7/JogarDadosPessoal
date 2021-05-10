1f365b6ffac37974ff5a4680db7f8787edb05ada
Gustavo Franco <gu.franco13@gmail.com>
2021-05-10 16:49:48 -0400

    DadoRandomComImagens

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var dadoImageView2: UIImageView!
     @IBOutlet weak var dadoImageView1: UIImageView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,9 +25,10 @@ class ViewController: UIViewController {
     
        
         
-        dadoImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][6]
+        dadoImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][Int.random(in: 0...5)]
+        dadoImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][Int.random(in: 0...5)]
         
-        dadoImageView2.image = #imageLiteral(resourceName: "DiceFour")
+ 
 
     }
 }
